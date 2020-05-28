cc88b84658594eeb3273836dc74e8cd46ee08ff8
bhusallaxman22 <laxmanbhusal612@gmail.com>
2020-05-28 15:04:13 +0545

    Updates

---
+++ precache-manifest.c3e62483e8c380e8911ec312e3f4d2bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e30a7035061c5da63fb83f679a89ae85",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8145ebe3bff2b32ec593",
+    "url": "/static/css/main.7b7d834e.chunk.css"
+  },
+  {
+    "revision": "b25d622ae120b5b73d1e",
+    "url": "/static/js/2.01b02a5d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.01b02a5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8145ebe3bff2b32ec593",
+    "url": "/static/js/main.5887d7da.chunk.js"
+  },
+  {
+    "revision": "bede3d04dfcdf73d34d8",
+    "url": "/static/js/runtime-main.e7e7ec22.js"
+  }
+]);
